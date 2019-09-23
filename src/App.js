2debaf53ebe9a0176b165910d748c6b02b9c4dfc
Dean Dijour <dijour@cmu.edu>
2019-09-23 10:16:23 -0400

    initial commit

--- src/App.js
+++ src/App.js
@@ -1,11 +1,16 @@
 import React from 'react';
 import logo from './logo.svg';
-import './App.css';
+import './App.scss';
+import { BrowserRouter as Router, Route, Link } from "react-router-dom";
+import Home from '../src/pages/home/home'
+import Index from '../src/pages/index/index'
+import Cart from '../src/pages/cart/cart'
+
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
+      {/* <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>
           Edit <code>src/App.js</code> and save to reload.
@@ -18,7 +23,12 @@ function App() {
         >
           Learn React
         </a>
-      </header>
+      </header> */}
+      <Router>
+        <Route path="/" exact component={Home} />
+        <Route path="/index/" component={Index} />
+        <Route path="/cart/" component={Cart} />
+      </Router>
     </div>
   );
 }
