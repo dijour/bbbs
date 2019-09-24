16c8b2de9bf818db5c3893580a5a6bdf806ea27f
Dean Dijour <dijour@cmu.edu>
2019-09-24 01:20:37 -0400

    home page done

--- src/pages/home/home.js
+++ src/pages/home/home.js
@@ -1,29 +1,40 @@
 import React, {useState, useEffect} from 'react';
+import bg from '../../assets/bg.svg'
+import styles from './home.module.scss'
 
 const Home = () => {
-    const [age, setAge] = useState(19)
-    const [siblingsNum, setSiblingsNum] = 
-        useState(10)
+    // const [age, setAge] = useState(19)
+    // const [siblingsNum, setSiblingsNum] = 
+    //     useState(10)
 
-    const handleAge = () => setAge(age + 1)
-    const handleSiblingsNum = () => 
-        setSiblingsNum(siblingsNum + 1)
+    // const handleAge = () => setAge(age + 1)
+    // const handleSiblingsNum = () => 
+    //     setSiblingsNum(siblingsNum + 1)
 
 
     return (
         <div>
-        <p>Today I am {age} Years of Age</p>
-        <p>I have {siblingsNum} siblings</p>
+            <div className={styles.heroText}>
+                <h1>Bun Bun Bake Shop</h1>
+                <button>Order Now!</button>
+            </div>
+            <div className={styles.fullPage}>
+            </div>
+        </div>
+        // <img src={bg} alt="background"></img>
+        // <div>
+        // <p>Today I am {age} Years of Age</p>
+        // <p>I have {siblingsNum} siblings</p>
 
-        <div>
-            <button onClick={handleAge}>
-            Get older! 
-            </button>
-            <button onClick={handleSiblingsNum}>
-                More siblings! 
-            </button>
-        </div>
-        </div>
+        // <div>
+        //     <button onClick={handleAge}>
+        //     Get older! 
+        //     </button>
+        //     <button onClick={handleSiblingsNum}>
+        //         More siblings! 
+        //     </button>
+        // </div>
+        // </div>
     )
 }
 
